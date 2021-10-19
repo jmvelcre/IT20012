9ec0cae0d678bd44b42c5ce20f1178ff1d2990c2
unknown <josemaria.velazquez.crespo@080formacion.alzago.es>
2021-10-19 18:08:14 +0200

    Final

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,10 @@ const inputEmail = document.getElementById("inputEmail");
 const imgError = document.getElementById("imgError");
 const btnEnviar = document.getElementById("btnEnviar");
 const msgError = document.getElementById("msgError");
-const formEmail = document.getElementById("formEmail")
+const formEmail = document.getElementById("formEmail");
 
 btnEnviar.onclick = function () {
-  if (
-    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputEmail.value)
-  ) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputEmail.value)) {
     imgError.style.display = "none";
     msgError.style.display = "none";
     formEmail.style.borderColor = "hsl(0, 36%, 70%)"
@@ -15,6 +13,5 @@ btnEnviar.onclick = function () {
     imgError.style.display = "block";
     msgError.style.display = "block";
     formEmail.style.borderColor = "hsl(0, 93%, 68%)";
-  }
-  
-};
+  } 
+}
